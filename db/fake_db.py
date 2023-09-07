345879276af6eefe527b84427c3cc3c73cc58d9e
Eivind Teig <eivind.teig@gmail.com>
2023-09-07 19:03:06 +0200

    Update fake_db.py
    Add my own user to the in memory database.

--- db/fake_db.py
+++ db/fake_db.py
@@ -6,4 +6,11 @@ fake_users_db = {
         "hashed_password": "$2b$12$oFkTWqjRoZ0QbwjpMHQFjutl2LLxYSC1Ff25sbkCwcMRylrtc.Usa",
         "disabled": False,
     },
+    "eivl": {
+        "username": "eivl",
+        "full_name": "Eivind Teig",
+        "email": "eivind.teig@gmail.com",
+        "hashed_password": "$2b$12$.funCY/x0gInEqWI9z7sRupxhawGC8pB02DAU8byU/Qs23zqDQY/G",
+        "disabled": False,
+    },
 }
